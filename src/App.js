2c88d85ad95ceea7849b7b88c61553d3fc06659c
Fernando Salamanca <lfernandosalamanca@gmail.com>
2022-03-19 21:28:18 +0100

    added appointment info component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BiCalendar } from 'react-icons/bi';
 import SearchBar from './components/SearchBar';
+import AddAppointment from './components/AddAppointment';
+import AppointmentInfo from './components/AppointmentInfo';
 
 function App() {
     return (
         <div className="App container mx-auto mt-3 font-thin">
-            <h1 className="text-5xl">
-                <BiCalendar className="inline-block text-red-400" />
+            <h1 className="text-5xl ">
+                <BiCalendar className="inline-block text-red-400 mr-3 align-top" />
                 Your Appointments
             </h1>
+            <AddAppointment />
             <SearchBar />
+            <AppointmentInfo />
         </div>
     );
 }
