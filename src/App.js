a1bf1053acf686008591a0a5cdc2964be278f66a
Fernando Salamanca <lfernandosalamanca@gmail.com>
2022-03-19 14:34:43 +0100

    created search component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BiCalendar } from 'react-icons/bi';
+import SearchBar from './components/SearchBar';
 
 function App() {
     return (
-        <div className="App">
-            <h1>
-                <BiCalendar />
+        <div className="App container mx-auto mt-3 font-thin">
+            <h1 className="text-5xl">
+                <BiCalendar className="inline-block text-red-400" />
                 Your Appointments
             </h1>
+            <SearchBar />
         </div>
     );
 }
