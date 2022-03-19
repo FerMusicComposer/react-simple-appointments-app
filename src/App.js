d0c13809cdc3c7a0e9dc87151ae9b3e1c48eba2f
Fernando Salamanca <lfernandosalamanca@gmail.com>
2022-03-19 13:58:22 +0100

    initial project config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BiCalendar } from 'react-icons/bi';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>
+                <BiCalendar />
+                Your Appointments
+            </h1>
+        </div>
+    );
 }
 
 export default App;
